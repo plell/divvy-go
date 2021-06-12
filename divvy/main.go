c213aef229569ec324c9cee9a696b1fc125cd6e3
David Plell <plelldavid@gmail.com>
2021-06-11 17:50:22 -0700

    s

--- divvy/main.go
+++ divvy/main.go
@@ -12,8 +12,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	core "github.com/plell/divvygo/divvy/core"
 	"github.com/robfig/cron/v3"
-	// "gorm.io/driver/mysql"
-	// "gorm.io/gorm"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
@@ -28,7 +27,7 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	// e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
+	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
 
 	// Middleware
 	e.Use(middleware.Logger())
@@ -62,10 +61,10 @@ func main() {
 	go core.RunWebsocketBroker()
 
 	// Start server
-	fmt.Println("start http 443 server!")
-	e.Logger.Fatal(e.Start(":443"))
+	// fmt.Println("start http 443 server!")
+	// e.Logger.Fatal(e.Start(":443"))
 
-	// fmt.Println("start tls 443 server!")
-	// e.Logger.Fatal(e.StartAutoTLS(":443"))
+	fmt.Println("start tls 443 server!")
+	e.Logger.Fatal(e.StartAutoTLS(":443"))
 
 }
